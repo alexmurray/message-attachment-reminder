975381d6e7c6771c462e73abd3398a4ed2a9b86b
Alex Murray <murray.alex@gmail.com>
2023-01-24 15:50:11 +1030

    Bump version to 0.2
    Signed-off-by: Alex Murray <murray.alex@gmail.com>

--- message-attachment-reminder.el
+++ message-attachment-reminder.el
@@ -5,7 +5,7 @@
 ;; Author: Alex Murray <murray.alex@gmail.com>
 ;; Maintainer: Alex Murray <murray.alex@gmail.com>
 ;; URL: https://github.com/alexmurray/message-attachment-reminder
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "24.1"))
 
 ;; This file is not part of GNU Emacs.
